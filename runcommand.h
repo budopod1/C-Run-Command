7f3ca918c951f1191e48e1a929a5e8d21b9ed9a9
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-10-28 11:39:31 -0700

    Added: header file

---
+++ runcommand.h
@@ -0,0 +1,39 @@
+#ifndef RUNCOMMAND_H
+#define RUNCOMMAND_H
+
+#include <stdint.h>
+
+union ___EPSL_PUBLIC_STOP;
+struct CRCProcessResult {
+    char *output;
+    unsigned char status;
+};
+union ___EPSL_PUBLIC_START;
+
+struct ARRAY_char {
+    uint64_t ref_counter;
+    uint64_t capacity;
+    uint64_t length;
+    char *content;
+}
+
+struct ARRAY_ARRAY_char {
+    uint64_t ref_counter;
+    uint64_t capacity;
+    uint64_t length;
+    struct EPSLString *content;
+}
+
+struct ProcessResult {
+    uint64_t ref_counter;
+    struct ARRAY_char *output;
+    uint8_t status;
+}
+
+union ___EPSL_PUBLIC_STOP;
+struct CRCProcessResult CRC_run_command(char *cmd, char **args, uint32_t arg_count);
+union ___EPSL_PUBLIC_START;
+
+struct ProcessResult *CRC_epsl_run_command(struct ARRAY_char *cmd, struct ARRAY_ARRAY_char *args);
+
+#endif
